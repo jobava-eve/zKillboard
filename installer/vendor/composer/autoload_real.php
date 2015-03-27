6040a526422e710ced39bfa0a0e16534ef65c14e
Michael Karbowiak <michael@karbowiak.dk>
2015-03-27 22:45:22 +0100

    Update composer and the config

--- installer/vendor/composer/autoload_real.php
+++ installer/vendor/composer/autoload_real.php
@@ -23,9 +23,6 @@ class ComposerAutoloaderInitab9c0f9214e989762c664c929c37e8d2
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitab9c0f9214e989762c664c929c37e8d2', 'loadClassLoader'));
 
-        $vendorDir = dirname(__DIR__);
-        $baseDir = dirname($vendorDir);
-
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
