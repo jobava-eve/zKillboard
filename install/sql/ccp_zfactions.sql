0158a8714d6d454eddbbc4ccdead9d2565fc5f76
Michael Karbowiak <karbowiak@gmail.com>
2015-03-27 21:17:06 +0100

    Update SQL tables

--- install/sql/ccp_zfactions.sql
+++ install/sql/ccp_zfactions.sql
@@ -6,3 +6,18 @@ CREATE TABLE `ccp_zfactions` (
   PRIMARY KEY (`factionID`),
   KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+LOCK TABLES `ccp_zfactions` WRITE;
+INSERT INTO `ccp_zfactions` VALUES ('500001', 'Caldari State', 'caldari');
+INSERT INTO `ccp_zfactions` VALUES ('500002', 'Minmatar Republic', 'minmatar');
+INSERT INTO `ccp_zfactions` VALUES ('500003', 'Amarr Empire', 'amarr');
+INSERT INTO `ccp_zfactions` VALUES ('500004', 'Gallente Federation', 'gallente');
+INSERT INTO `ccp_zfactions` VALUES ('500007', 'Ammatar Mandate', '');
+INSERT INTO `ccp_zfactions` VALUES ('500010', 'Guristas Pirates', '');
+INSERT INTO `ccp_zfactions` VALUES ('500011', 'Angel Cartel', '');
+INSERT INTO `ccp_zfactions` VALUES ('500012', 'Blood Raider Covenant', '');
+INSERT INTO `ccp_zfactions` VALUES ('500018', 'Mordu's Legion Command', '');
+INSERT INTO `ccp_zfactions` VALUES ('500019', 'Sansha's Nation', '');
+INSERT INTO `ccp_zfactions` VALUES ('500020', 'Serpentis', '');
+INSERT INTO `ccp_zfactions` VALUES ('500021', 'Unknown', '');
+
+UNLOCK TABLES;
